7b7dcde8d93331f766bb092cdd0aa7458b57a1e4
Anton <weymar87@gmail.com>
2022-02-09 21:49:36 +0500

    Relocate SOUT with message about New connection

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,13 +8,14 @@ import java.net.Socket;
 public class Main {
     public static void main (String[] args) throws IOException {
         int port = 8089;
+        System.out.println("New connection accepted");
 
         ServerSocket serverSocket = new ServerSocket(port);
         Socket clientSocket = serverSocket.accept();
         PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
         BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-        System.out.println("New connection accepted");
+
 
         final String name = in.readLine();
 
